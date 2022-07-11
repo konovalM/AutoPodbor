52e0c213cbd78ad0e1f734638568fafd279d2e68
konovalM <konovalenko.ii4elka@mail.ru>
2022-07-11 14:42:56 +0300

    try/catch w/ tel link

--- src/functions/deleteSpan.js
+++ src/functions/deleteSpan.js
@@ -1,9 +1,15 @@
 export function deleteSpan(content){
-    let phone = content
-    const start = content.indexOf('+')
-    phone = phone.substring(start)
-    const end = phone.indexOf('<')
-    phone = phone.substring(0, end)
-    phone = phone.split(' ').join('').split('-').join('').split('(').join('').split(')').join('')
-    localStorage.setItem('tel', phone)
+    try {
+        let phone = content
+        const start = content.indexOf('+')
+        phone = phone.substring(start)
+        const end = phone.indexOf('<')
+        phone = phone.substring(0, end)
+        phone = phone.split(' ').join('').split('-').join('').split('(').join('').split(')').join('')
+        localStorage.setItem('tel', phone)
+    } catch (e){
+        console.log(e)
+        localStorage.setItem('tel', '+78122198586')
+    }
+
 }
