fb4b6f3c88e5a4647338fadd42676d6833c74e69
konovalM <konovalenko.ii4elka@mail.ru>
2022-08-21 20:32:30 +0300

    added visual elements

--- src/components/Abroad/Calculator/Calculator.js
+++ src/components/Abroad/Calculator/Calculator.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './Calculator.module.css'
 import FormCalculator from "../../FormCalculator/FormCalculator";
+import notebook from '../../../assets/images/abroad/penAndNotebook.png'
+import calculator from '../../../assets/images/abroad/CalcAndCash.png'
 
 const Calculator = () => {
     return (
         <section className={styles.main}>
+            <img src={notebook} alt="notebook" className={styles.notebook}/>
+            <img src={calculator} alt="calculator" className={styles.calculator}/>
             <div className="container">
                 <h5 className={styles.title}>Калькулятор растаможки</h5>
                 <FormCalculator />
