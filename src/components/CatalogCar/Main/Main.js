539df6814833306ac19fa20776cd7d9ab56e00d3
konovalM <konovalenko.ii4elka@mail.ru>
2022-08-30 08:37:44 +0300

    fix: change static car name

--- src/components/CatalogCar/Main/Main.js
+++ src/components/CatalogCar/Main/Main.js
@@ -25,7 +25,7 @@ const Main = ({car}) => {
         <section className={styles.section}>
             <div className="container">
                 <h6 className={styles.carName}>
-                    Toyota Land Cruiser 300
+                    {car?.title}
                 </h6>
                 <div className={styles.card}>
                     <div className={styles.imgWrapper}>
