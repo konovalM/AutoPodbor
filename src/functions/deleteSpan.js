608b7e104c7cc2116a0255cff638e83b39179560
DankAvHi <thexcat86@gmail.com>
2023-08-29 12:01:19 +0300

    change fallback phone

--- src/functions/deleteSpan.js
+++ src/functions/deleteSpan.js
@@ -2,6 +2,6 @@ export function deleteSpan(content){
     try{
         localStorage.setItem('tel', content.match(/[+\d]/g).join(''))
     } catch (e){
-        localStorage.setItem('tel', '+78122198586')
+        localStorage.setItem('tel', '+78124256088')
     }
 }
